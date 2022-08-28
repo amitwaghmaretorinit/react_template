c8f52cfc51f77fd2e05526a1c0cd4d9151881d93
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2022-08-28 13:42:46 +0530

    minor changes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,12 @@
 import { combineReducers, compose, createStore } from "redux";
 import authReducer from "./authReducer";
-import staffReducer from "./staffReducer";
 import userProfileReducer from "./userProfileReducer";
 import rawDataReducer from "./rawDataReducer";
-import agentOnboardingReducer from "./agentOnboardingReducer";
-
+ 
 const reducer = combineReducers({
   auth: authReducer,
   user: userProfileReducer,
-  stff: staffReducer,
-  rawData: rawDataReducer,
-  agentOnboarding: agentOnboardingReducer,
+ 
 });
 //@ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
