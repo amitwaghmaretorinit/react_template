7653df7879820932cbbb68d824999f0fa2e08c32
amitwaghmaretorinit <79451605+amitwaghmaretorinit@users.noreply.github.com>
2022-08-28 13:59:16 +0530

    minor changes

--- src/configs/routes.tsx
+++ src/configs/routes.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { Flex } from '@atom/Flex';
 import HomePage from '@pages/HomePage';
 
- 
 const RoutesComponent = () => {
 
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} /> 
-    </Routes>
+    <Suspense fallback={<Splash />}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Suspense>
   );
 };
+
+const Splash = () => {
+  return <Flex direction='column' justifyContent={'center'} style={{ height: '100vh' }}>
+    <Flex justifyContent={'center'}>
+      <img src='https://torinit.com/static/media/loader.e1dc4171.gif' style={{ width: 300 }} />
+    </Flex>
+  </Flex>
+}
 
 export default RoutesComponent;
